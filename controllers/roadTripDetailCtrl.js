d0b78be36207eeb06048bad44e818093228ba58b
Adrian Pomilio <adrianpomilio@gmail.com>
2013-07-20 23:01:22 -0400

    added basic navigation and layout

---
+++ controllers/roadTripDetailCtrl.js
@@ -0,0 +1,46 @@
+function RoadTripDetailCtrl($scope, $routeParams) {
+  $scope.roadTrips = [
+  	{tripId:1, 
+  	tripName:"Asheville", 
+  	tripDescription:"Lacrosse Tournament",
+  	tripOwner:"Adrian",
+  	tripStartDate:"July 27, 2013",
+  	tripEndDate:"July 29, 2013",
+  	people:[
+  		{name:"Adrian",
+  		phone:"919-656-0380",
+  		currentlocation: {lat:0, lon:0},
+  		pastLocations:[]
+  		},
+  		{name:"Mike",
+  		phone:"919-656-0380",
+  		currentlocation: {lat:0, lon:0},
+  		pastLocations:[]
+  		},
+  		{name:"Turbo",
+  		phone:"919-656-0380",
+  		currentlocation: {lat:0, lon:0},
+  		pastLocations:[]
+  		}
+  	]
+  }
+  ];
+  
+  $scope.trip = getTrip($routeParams.tripId);
+
+
+  function getTrip(tripId){
+
+    var len =  $scope.roadTrips.length,
+        trip = {};
+        for(var i = 0; i < len; i++){
+          if($scope.roadTrips[i].tripId == tripId){
+            
+            return trip = $scope.roadTrips[i];
+          }
+        }
+    return trip;
+  }
+};
+ 
+RoadTripDetailCtrl.$inject = ['$scope', '$routeParams'];
--- js/app.js
+++ js/app.js
@@ -1,30 +1,27 @@
-$( document ).ready(function() {
-  		// reference to my data at firebase 
-	      var fbRef = new Firebase("https://road-tripper.firebaseio.com/");
-	      // set up of authentication
-	      var auth = new FirebaseSimpleLogin(fbRef, function(error, user) {
-			  if (error) {
+angular.module('roadtripper', ['ui.bootstrap']).
+  config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+      when('/trips', {templateUrl: 'partials/road-trips.html', controller: RoadTripsCtrl}).
+      when('/trip/:tripId', {templateUrl: 'partials/road-trip-details.html', controller: RoadTripDetailCtrl}).
+      otherwise({redirectTo: '/trips'});
+}]);
+
+/*
+var fbRef = new Firebase("https://road-tripper.firebaseio.com/");
+var userID = null;
+
+var auth = new FirebaseSimpleLogin(commentsRef, function(error, user) {
+  if (error) {
 			    // an error occurred while attempting login
 			    console.log(error);
 			  } else if (user) {
 			    // user authenticated with Firebase
 			    console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+
 			  } else {
-			    
+			     userID = user.id;
+
 			  }
-			});
-
-	     
-      });
-
-auth.login('password', {
-						  email: '<email@domain.com>',
-						  password: '<password>',
-						  rememberMe: true
-						});
-
-auth.changePassword(email, oldPassword, newPassword, function(error, success) {
-			  if (!error) {
-			    console.log('Password change successfully');
-			  }
-			});
+  
+});
+*/	     
