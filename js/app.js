33369433d0ae827f1bbacb89f968ffc9d9fe3948
Adrian Pomilio <adrianpomilio@gmail.com>
2013-07-20 20:44:04 -0400

    begining firebase authentication

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-app.js
+$( document ).ready(function() {
+  		// reference to my data at firebase 
+	      var fbRef = new Firebase("https://road-tripper.firebaseio.com/");
+	      // set up of authentication
+	      var auth = new FirebaseSimpleLogin(fbRef, function(error, user) {
+			  if (error) {
+			    // an error occurred while attempting login
+			    console.log(error);
+			  } else if (user) {
+			    // user authenticated with Firebase
+			    console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+			  } else {
+			    
+			  }
+			});
+
+	     
+      });
+
+auth.login('password', {
+						  email: '<email@domain.com>',
+						  password: '<password>',
+						  rememberMe: true
+						});
+
+auth.changePassword(email, oldPassword, newPassword, function(error, success) {
+			  if (!error) {
+			    console.log('Password change successfully');
+			  }
+			});
