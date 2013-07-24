57501239f44e0b62779ea3c465e5bb6da206c366
Adrian Pomilio <adrianpomilio@gmail.com>
2013-07-23 20:48:09 -0400

    reading from firebase now, list and detail read

--- controllers/roadTripsCtrl.js
+++ controllers/roadTripsCtrl.js
@@ -1,30 +1,9 @@
-function RoadTripsCtrl($scope, $routeParams) {
-  $scope.roadTrips = [
-  	{tripId:1, 
-  	tripName:"Asheville", 
-  	tripDescription:"Lacrosse Tournament",
-  	tripOwner:"Adrian",
-  	tripStartDate:"July 27, 2013",
-  	tripEndDate:"July 29, 2013",
-  	people:[
-  		{name:"Adrian",
-  		phone:"919-656-0380",
-  		currentlocation: {lat:0, lon:0},
-  		pastLocations:[]
-  		},
-  		{name:"Mike",
-  		phone:"919-656-0380",
-  		currentlocation: {lat:0, lon:0},
-  		pastLocations:[]
-  		},
-  		{name:"Turbo",
-  		phone:"919-656-0380",
-  		currentlocation: {lat:0, lon:0},
-  		pastLocations:[]
-  		}
-  	]
-  }
-  ]
-};
+function RoadTripsCtrl($scope, $routeParams, angularFireCollection) {
+	var url = 'https://road-tripper.firebaseio.com/trips';
+	$scope.trips = angularFireCollection(url);
+	
+    console.log($scope.trips)
+    
+ };
  
-RoadTripsCtrl.$inject = ['$scope', '$routeParams'];
+RoadTripsCtrl.$inject = ['$scope', '$routeParams','angularFireCollection'];
